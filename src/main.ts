4b7b32fe39d1026ade8f4397e40d8854f5400a96
Botond Sisak <sisak.botond@gmail.com>
2024-04-21 22:32:01 +0200

    reset db endpoint

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ import trafficRouter from "./traffic/traffic.router";
 import loginRouter from "./login/login.router";
 import { createRouter } from "./utils/router";
 import { db } from "./utils/db";
+import fs from "fs";
 
 const app = express();
 const port = 5000; // Port where the app will listen
@@ -25,6 +26,53 @@ app.use((_, res, next) => {
 const apiRoutes = createRouter()
 
 apiRoutes.use(loginRouter)
+
+apiRoutes.post("/reset-db", async (req, res) => {
+  try {
+		// ignore foreign key checks
+		await db.$executeRawUnsafe("SET FOREIGN_KEY_CHECKS = 0;")
+		await db.$executeRawUnsafe("DROP TABLE IF EXISTS `MenuCategory`")
+		await db.$executeRawUnsafe("DROP TABLE IF EXISTS `MenuItem`")
+		await db.$executeRawUnsafe("DROP TABLE IF EXISTS `Order`")
+		await db.$executeRawUnsafe("DROP TABLE IF EXISTS `OrderItem`")
+		await db.$executeRawUnsafe("DROP TABLE IF EXISTS `Table`")
+		await db.$executeRawUnsafe("DROP TABLE IF EXISTS `User`")
+    await db.$executeRawUnsafe("DROP TABLE IF EXISTS `_prisma_migrations`")
+		await db.$executeRawUnsafe("SET FOREIGN_KEY_CHECKS = 1;")
+
+		console.log("Loading dump from:")
+		console.log(process.cwd() + "/dineease.sql")
+
+		//  load dump.sql and run it
+		const dump = fs
+			.readFileSync(process.cwd() + "/dineease.sql", "utf-8")
+      .replace(/\r\n/g, "\n")
+			.split("\n")
+			.filter(Boolean)
+			.filter((line) => !line.startsWith("--"))
+			.join(" ")
+			.split(";")
+			.filter(Boolean)
+
+		for (const query of dump) {
+      if (query.trim().startsWith("START TRANSACTION") || query.trim().startsWith("COMMIT") || query.trim() === "") {
+        continue
+      }
+      try {
+        await db.$executeRawUnsafe(query.trim() + ";")
+      } catch (error) {
+        console.error("Error executing query: ", query)
+        console.error(error)
+        throw error
+      }
+		}
+		console.log("The database reset was successful")
+		res.send({ message: "The database reset was successful" })
+	} catch (error) {
+		console.error(error)
+		res.status(500).send({ error: "Database reset failed" })
+	}
+})
 
 apiRoutes.use((req, res, next) => {
   if (req.method === "OPTIONS") {
