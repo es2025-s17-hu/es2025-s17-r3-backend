30feb8501595d0119eff9f7cecee21c0522bb543
Botond Sisak <sisak.botond@gmail.com>
2024-04-24 21:45:33 +0200

    create order item validation

--- src/orderItem/orderItem.service.ts
+++ src/orderItem/orderItem.service.ts
@@ -4,6 +4,13 @@ import { db } from "../utils/db";
 class OrderItemService {
 
   async createOrderItem(orderItem: OrderItem) {
+    const order = await db.order.findUnique({
+      where: { id: orderItem.orderId },
+    });
+    console.log(order)
+    if (!order || order.closedAt) {
+      throw new Error("Order not found or closed.");
+    }
     return await db.orderItem.create({
       data: orderItem,
     });
